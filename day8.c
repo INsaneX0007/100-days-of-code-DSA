5b618ae6c9b28b2dd5c06daee5ef5081afc136af
INsaneX0007 <rmei3737@gmail.com>
2026-02-18 19:51:00 +0530

    Solve Day 8: day8.c

---
+++ day8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+long long power(int a, int b) {
+    if (b == 0) return 1;
+    if (b == 1) return a;
+    
+    long long half = power(a, b / 2);
+    if (b % 2 == 0) {
+        return half * half;
+    } else {
+        return a * half * half;
+    }
+}
+
+int main() {
+    int a, b;
+    if (scanf("%d %d", &a, &b) != 2) return 1;
+    
+    printf("%lld\n", power(a, b));
+    return 0;
+}
