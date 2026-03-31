a04509d80148f4e986eff3211d717eea9dee6641
INsaneX0007 <rmei3737@gmail.com>
2026-03-31 20:10:00 +0530

    Solve Day 57: day57.c

---
+++ day57.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int data) {
+    if (data == -1) return NULL;
+    struct Node* nNode = (struct Node*)malloc(sizeof(struct Node));
+    nNode->data = data;
+    nNode->left = nNode->right = NULL;
+    return nNode;
+}
+
+struct Node* build(int arr[], int i, int n) {
+    if (i < n && arr[i] != -1) {
+        struct Node* root = createNode(arr[i]);
+        root->left = build(arr, 2 * i + 1, n);
+        root->right = build(arr, 2 * i + 2, n);
+        return root;
+    }
+    return NULL;
+}
+
+void mirror(struct Node* node) {
+    if (node == NULL) return;
+    struct Node* temp = node->left;
+    node->left = node->right;
+    node->right = temp;
+    mirror(node->left);
+    mirror(node->right);
+}
+
+void inorder(struct Node* node) {
+    if (node == NULL) return;
+    inorder(node->left);
+    printf("%d ", node->data);
+    inorder(node->right);
+}
+
+int main() {
+    int n, arr[1000];
+    if (scanf("%d", &n) != 1) return 1;
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    struct Node* root = build(arr, 0, n);
+    mirror(root);
+    inorder(root);
+    printf("\n");
+    return 0;
+}
