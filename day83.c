2a027e84fd83a163517cf2736dbd3e3a494f2c86
INsaneX0007 <rmei3737@gmail.com>
2026-04-16 13:43:00 +0530

    Solve Day 83: day83.c

---
+++ day83.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i) {
+            swap(&arr[min_idx], &arr[i]);
+        }
+    }
+}
+
+int main() {
+    int n, arr[1000];
+    if (scanf("%d", &n) != 1) return 1;
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    
+    selectionSort(arr, n);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) printf(" ");
+    }
+    printf("\n");
+    return 0;
+}
