83c240bab7ac19842aa9778e96e010a639db496d
INsaneX0007 <rmei3737@gmail.com>
2026-04-24 20:10:00 +0530

    Solve Day 93: day93.c

---
+++ day93.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n, arr[1000];
+    if (scanf("%d", &n) != 1) return 0;
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) printf(" ");
+    }
+    printf("\n");
+    return 0;
+}
