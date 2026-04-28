9e51334f2f70c4c415c68c9e8673309c690216fc
INsaneX0007 <rmei3737@gmail.com>
2026-04-28 15:07:00 +0530

    Solve Day 97: day97.c

---
+++ day97.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Meeting {
+    int start;
+    int end;
+};
+
+int compare(const void* a, const void* b) {
+    return (((struct Meeting*)a)->end - ((struct Meeting*)b)->end);
+}
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+    struct Meeting meetings[1000];
+    for (int i = 0; i < n; i++) scanf("%d %d", &meetings[i].start, &meetings[i].end);
+    qsort(meetings, n, sizeof(struct Meeting), compare);
+    int count = 1;
+    int last_end = meetings[0].end;
+    for (int i = 1; i < n; i++) {
+        if (meetings[i].start > last_end) {
+            count++;
+            last_end = meetings[i].end;
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}
